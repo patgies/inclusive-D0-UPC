34dcc6908991836b4ea985bd0c94b4b084532c55
patricia <pagimeno@jyu.fi>
2026-07-16 15:51:13 +0300

    updated run scripts

---
+++ run_local.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Run ./build/bin/dipole at a single rapidity, sweeping pD0, for one dipole file.
+# Writes one file: pD0  dsigma_dydptd
+
+BIN=./build/bin/dipole
+DIPOLE_FILE=./data/proton/mve.dat
+Y=1.0
+OUTDIR=./out
+
+mkdir -p "$OUTDIR"
+
+outfile="$OUTDIR/spectrum_y${Y}.dat"
+echo "# pD0  dsigma_dydpt (y=$Y)" > "$outfile"
+
+for pt in $(seq 0.1 0.2 12.0); do
+    echo "pD0=$pt"
+    val=$("$BIN" "$pt" "$DIPOLE_FILE" "$Y" | awk '$1 !~ /^#/ {print $2}')
+    echo "$pt  $val" >> "$outfile"
+done
